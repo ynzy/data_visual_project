9028fb36864e6ba08a06c32186f63471e8bf0891
张勇 <758922096@qq.com>
2020-10-07 08:03:26 +0800

    feat(model:all): 销售总额

--- data-report/src/components/TopView/TotalSales/index.vue
+++ data-report/src/components/TopView/TotalSales/index.vue
@@ -1,42 +1,53 @@
 <!-- 销售总额 -->
 <template>
-	<div>
-		<CommonCard title="累计销售额" value="1123">
-			<template>
-				<div class="compare">表哥</div>
-			</template>
-			<template v-slot:footer>
-				<div><span>昨日销售额</span> <span class="money">¥ 234234234234</span></div>
-			</template>
-		</CommonCard>
-	</div>
+<div>
+  <CommonCard title="累计销售额" value="1123">
+    <template>
+      <div class="compare-wrapper">
+        <div class="compare">
+          <span>日同比</span><span class="emphasis">7.33%</span>
+          <div class="increase"></div>
+        </div>
+        <div class="compare">
+          <span>月同比</span><span class="emphasis">7.33%</span>
+          <div class="decrease"></div>
+        </div>
+      </div>
+    </template>
+    <template v-slot:footer> <span>昨日销售额</span> <span class="emphasis">¥ 234234234234</span> </template>
+  </CommonCard>
+</div>
 </template>
 
 <script>
 import commonCardMixin from '../../../mixins/commonCardMixin'
 
 export default {
-	mixins: [commonCardMixin],
+  mixins: [commonCardMixin],
 
-	data() {
-		return {}
-	},
-	methods: {},
-	mounted() {},
-	components: {}
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {},
+  components: {}
 }
 </script>
+
 <style lang="scss" scoped>
+.compare-wrapper {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
 .compare {
-	height: 100%;
-	background: yellow;
-}
-span {
-	font-size: 12px;
-}
-.money {
-	margin-left: 5px;
-	color: #333;
-	font-weight: bold;
+  display: flex;
+  align-items: center;
+  margin-top: 3px;
+  font-size: 12px;
+  color: #666;
+  background: yellow;
 }
 </style>
