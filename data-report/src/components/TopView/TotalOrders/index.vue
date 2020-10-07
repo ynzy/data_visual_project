086fe70c6a4e90c141e9e4c0cce3612b2629fc74
张勇 <758922096@qq.com>
2020-10-07 08:33:42 +0800

    feat(model:all): 样式优化

--- data-report/src/components/TopView/TotalOrders/index.vue
+++ data-report/src/components/TopView/TotalOrders/index.vue
@@ -1,24 +1,67 @@
-<!-- 销售总额 -->
+<!-- 订单总额 -->
 <template>
-	<div>
-		<CommonCard title="累计销售额" value="1123"></CommonCard>
-	</div>
+<div>
+  <CommonCard title="累计订单量" value="3,324,073">
+    <template>
+      <div id="total-order-chart" :style="{ width: '100%', height: '100%' }"></div>
+    </template>
+    <template v-slot:footer>
+      <span>昨日订单量</span>
+      <span class="emphasis">¥ 2,000,000</span>
+    </template>
+  </CommonCard>
+</div>
 </template>
 
 <script>
 import commonCardMixin from '../../../mixins/commonCardMixin'
 
 export default {
-	mixins: [commonCardMixin],
+  mixins: [commonCardMixin],
 
-	data() {
-		return {}
-	},
-	methods: {},
-	mounted() {},
-	components: {}
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+    const chartDom = document.getElementById('total-order-chart')
+    const chart = this.$echarts.init(chartDom, 'light', {
+      renderer: 'svg'
+    })
+    chart.setOption({
+      xAxis: {
+        type: 'category', // 分类
+        show: false,
+        boundaryGap: false // 坐标轴两旁留白
+      },
+      yAxis: {
+        show: false
+      },
+      series: [{
+        type: 'line',
+        data: [620, 432, 220, 534, 790, 430, 220, 320, 532, 320, 834, 690, 530, 220, 620],
+        areaStyle: {
+          // 区域样式
+          color: 'purple'
+        },
+        lineStyle: {
+          // 线的样式
+          width: 0
+        },
+        itemStyle: {
+          // 点的样式
+          opacity: 0
+        },
+        smooth: true // 是否平顺
+      }],
+      grid: {
+        top: 0,
+        right: 0,
+        left: 0,
+        bottom: 0
+      }
+    })
+  },
+  components: {}
 }
 </script>
-<style lang="scss" scoped>
-/* @import url(); 引入css类 */
-</style>
--- data-report/src/components/TopView/TotalSales/index.vue
+++ data-report/src/components/TopView/TotalSales/index.vue
@@ -1,20 +1,25 @@
 <!-- 销售总额 -->
 <template>
 <div>
-  <CommonCard title="累计销售额" value="1123">
+  <CommonCard title="累计销售额" value="54,032,530">
     <template>
       <div class="compare-wrapper">
         <div class="compare">
-          <span>日同比</span><span class="emphasis">7.33%</span>
+          <span>日同比</span>
+          <span class="emphasis">80.11% %</span>
           <div class="increase"></div>
         </div>
         <div class="compare">
-          <span>月同比</span><span class="emphasis">7.33%</span>
+          <span>月同比</span>
+          <span class="emphasis">132.90%</span>
           <div class="decrease"></div>
         </div>
       </div>
     </template>
-    <template v-slot:footer> <span>昨日销售额</span> <span class="emphasis">¥ 234234234234</span> </template>
+    <template v-slot:footer>
+      <span>昨日销售额</span>
+      <span class="emphasis">¥ 30,000,000</span>
+    </template>
   </CommonCard>
 </div>
 </template>
@@ -48,6 +53,5 @@ export default {
   margin-top: 3px;
   font-size: 12px;
   color: #666;
-  background: yellow;
 }
 </style>
