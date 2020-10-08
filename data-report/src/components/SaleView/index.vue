08c96083b0a7faf97fef636d1e5086d43967b91d
张勇 <758922096@qq.com>
2020-10-08 18:31:20 +0800

    feat(model:all): 销售列表，组件容器头部

--- data-report/src/components/SaleView/index.vue
+++ data-report/src/components/SaleView/index.vue
@@ -1,8 +1,42 @@
 <!--  -->
 <template>
-  <v-chart
-    :options="chartOptions"
-  />
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu
+            class="el-menu-sales-view"
+            :default-active="activeIndex"
+            mode="horizontal"
+            @select="onMenuSelect"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+         <div class="menu-right">
+          <el-radio-group v-model="radioSelect" size="small" >
+            <el-radio-button label="今日"></el-radio-button>
+            <el-radio-button label="本周"></el-radio-button>
+            <el-radio-button label="本月"></el-radio-button>
+            <el-radio-button label="今年"></el-radio-button>
+          </el-radio-group>
+           <el-date-picker
+             class="sales-view-date"
+            type="daterange"
+            size="small"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+             unlink-panels
+            v-model="date"
+            :picker-options="pickerOptions"
+           />
+         </div>
+        </div>
+      </template>
+
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -10,19 +44,43 @@
 export default {
   data() {
     return {
-      chartOptions: {
-        xAxis: {
-          type: "category"
-        },
-        yAxis: {},
-        series: [{
-          type: "line",
-          data: [100,200,300]
+      activeIndex:'1',
+      radioSelect: '今日',
+      date: null,
+      pickerOptions: {
+        shortcuts: [{
+          text: '最近一周',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 7);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近一个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 30);
+            picker.$emit('pick', [start, end]);
+          }
+        }, {
+          text: '最近三个月',
+          onClick(picker) {
+            const end = new Date();
+            const start = new Date();
+            start.setTime(start.getTime() - 3600 * 1000 * 24 * 90);
+            picker.$emit('pick', [start, end]);
+          }
         }]
-      }
+      },
     }
   },
-  methods: {},
+  methods: {
+    onMenuSelect(index) {
+      this.activeIndex = index
+    }
+  },
   mounted() {
   },
   components: {}
@@ -30,5 +88,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.sales-view {
+  margin-top:20px;
+  .menu-wrapper {
+    position: relative;
+    .el-menu-sales-view {
+      padding-left: 20px;
+    }
+    .el-menu-item {
+      margin: 0 20px;
+      height:50px;
+      line-height:50px;
+    }
+    .menu-right {
+      position: absolute;
+      top: 0;
+      right: 20px;
+      display: flex;
+      align-items: center;
+      height:50px;
+      .sales-view-date {
+        margin-left: 20px;
+      }
+    }
+  }
+}
 
 </style>
--- data-report/src/main.js
+++ data-report/src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './assets/css/index.scss'
 import './plugins/element.js'
 import './plugins/vcharts'
 import ECharts from 'echarts'
 import VueECharts from 'vue-echarts'
+
+import './assets/css/index.scss'
 Vue.config.productionTip = false
 
 Vue.prototype.$echarts = ECharts
