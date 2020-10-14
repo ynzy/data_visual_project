3cc8552343a8cbfd1efb6c1bd23ba39551c2aee8
zhangyong <758922096@qq.com>
2020-10-14 14:39:22 +0800

    feat(model:all): 百度地图，大盘数据

--- data-report/src/views/BMap.vue
+++ data-report/src/views/BMap.vue
@@ -1,6 +1,11 @@
 <!--  -->
 <template>
-  <v-chart :options="options" />
+  <v-chart
+    ref="echart"
+    :options="options"
+    @finished="finished"
+    @click="handleClick"
+  />
 </template>
 
 <script>
@@ -33,98 +38,115 @@ export default {
   components: {},
   data() {
     return {
-      options: {}
+      options: {
+        title: {
+          text: '外卖销售数据大盘',
+          subtext: '销售趋势统计',
+          sublink: 'https://www.baidu.com',
+          left: 'center'
+        },
+        bmap: {
+          key: 'FkCqbmKtfHsKPtnEjvhjFcg6pwlN03jO',
+          center: [116.404, 39.915],
+          zoom: 5,
+          roam: true,
+          mapStyle: { style: 'grayscale' }
+        },
+        tooltip: {},
+        series: [
+          {
+            name: '销售额',
+            type: 'scatter',
+            coordinateSystem: 'bmap', // 坐标系统
+            data: convertData(scatterData),
+            encode: {
+              value: 2 //定制value值
+            },
+            itemStyle: {
+              color: 'purple'
+            },
+            symbolSize: function(val) {
+              //点的大小
+              return val[2] / 10
+            },
+            label: {
+              show: false,
+              position: 'right',
+              formatter: function(v) {
+                return `${v.data.name} - ${v.data.value[2]}`
+              }
+            },
+            // 鼠标移入显示label
+            emphasis: {
+              label: {
+                show: true
+              }
+            }
+          },
+          {
+            name: 'Top 2',
+            type: 'effectScatter', //波纹散点
+            coordinateSystem: 'bmap', // 坐标系统
+            data: convertData(
+              scatterData.sort(function(a, b) {
+                return b.value - a.value
+              })
+            ).slice(0, 10),
+            encode: {
+              value: 2 //定制value值
+            },
+            itemStyle: {
+              color: 'purple',
+              shadowBlur: 10,
+              shadowColor: '#333'
+            },
+            symbolSize: function(val) {
+              //点的大小
+              return val[2] / 10
+            },
+            label: {
+              show: false,
+              position: 'right',
+              formatter: function(v) {
+                return `${v.data.name} - ${v.data.value[2]}`
+              }
+            },
+            // 鼠标移入显示label
+            emphasis: {
+              label: {
+                show: true
+              }
+            },
+            hoverAnimation: true,
+            // 波纹样式
+            rippleEffect: {
+              brushType: 'stroke'
+            }
+          }
+        ]
+      }
     }
   },
-  methods: {},
-  mounted() {
-    this.options = {
-      title: {
-        text: '外卖销售数据大盘',
-        subtext: '销售趋势统计',
-        sublink: 'https://www.baidu.com',
-        left: 'center'
-      },
-      bmap: {
-        key: 'FkCqbmKtfHsKPtnEjvhjFcg6pwlN03jO',
-        center: [116.404, 39.915],
-        zoom: 5,
-        roam: true,
-        mapStyle
-      },
-      tooltip: {},
-      series: [
-        {
-          name: '销售额',
-          type: 'scatter',
-          coordinateSystem: 'bmap', // 坐标系统
-          data: convertData(scatterData),
-          encode: {
-            value: 2 //定制value值
-          },
-          itemStyle: {
-            color: 'purple'
-          },
-          symbolSize: function(val) {
-            //点的大小
-            return val[2] / 10
-          },
-          label: {
-            show: false,
-            position: 'right',
-            formatter: function(v) {
-              return `${v.data.name} - ${v.data.value[2]}`
-            }
-          },
-          // 鼠标移入显示label
-          emphasis: {
-            label: {
-              show: true
-            }
-          }
-        },
-        {
-          name: 'Top 2',
-          type: 'effectScatter', //波纹散点
-          coordinateSystem: 'bmap', // 坐标系统
-          data: convertData(
-            scatterData.sort(function(a, b) {
-              return b.value - a.value
-            })
-          ).slice(0, 10),
-          encode: {
-            value: 2 //定制value值
-          },
-          itemStyle: {
-            color: 'purple',
-            shadowBlur: 10,
-            shadowColor: '#333'
-          },
-          symbolSize: function(val) {
-            //点的大小
-            return val[2] / 10
-          },
-          label: {
-            show: false,
-            position: 'right',
-            formatter: function(v) {
-              return `${v.data.name} - ${v.data.value[2]}`
-            }
-          },
-          // 鼠标移入显示label
-          emphasis: {
-            label: {
-              show: true
-            }
-          },
-          hoverAnimation: true,
-          // 波纹样式
-          rippleEffect: {
-            brushType: 'stroke'
-          }
-        }
-      ]
+  methods: {
+    handleClick() {
+      console.log('111')
+    },
+    finished(e) {
+      console.log(e)
+      console.log('完成')
     }
+  },
+  mounted() {
+    /* const chart = this.$refs['echart'].chart
+    chart.on('finished', () => {
+      // 从echarts对象中获取bmap对象
+      var bmap = chart
+        .getModel()
+        .getComponent('bmap')
+        .getBMap()
+      console.log(bmap)
+    })
+    console.log(chart) */
   }
 }
 </script>
---
+++ data-report/src/views/BMap2.vue
@@ -0,0 +1,136 @@
+<!--  -->
+<template>
+  <ve-bmap
+    :settings="chartSettings"
+    :title="title"
+    :series="series"
+    :tooltip="tooltip"
+    :after-set-option-once="afterSet"
+    height="100%"
+  >
+  </ve-bmap>
+</template>
+
+<script>
+import mapStyle from '../utils/mapStyle.json'
+import { scatterData, geoCoordMap } from '../utils/scatterData'
+
+const convertData = function(data) {
+  let res = []
+  data.forEach(item => {
+    const { name, value } = item
+    const coord = geoCoordMap[name]
+    res.push({
+      name,
+      value: [...coord, value]
+    })
+  })
+  return res
+}
+
+export default {
+  components: {},
+  data() {
+    return {
+      title: {
+        text: '外卖销售数据大盘',
+        subtext: '销售趋势统计',
+        sublink: 'https://www.baidu.com',
+        left: 'center'
+      },
+      chartSettings: {
+        key: 'FkCqbmKtfHsKPtnEjvhjFcg6pwlN03jO',
+        bmap: {
+          center: [116.404, 39.915],
+          zoom: 5,
+          roam: true,
+          mapStyle
+        }
+      },
+      tooltip: {},
+      series: [
+        {
+          name: '销售额',
+          type: 'scatter',
+          coordinateSystem: 'bmap', // 坐标系统
+          data: convertData(scatterData),
+          encode: {
+            value: 2 //定制value值
+          },
+          itemStyle: {
+            color: 'purple'
+          },
+          symbolSize: function(val) {
+            //点的大小
+            return val[2] / 10
+          },
+          label: {
+            show: false,
+            position: 'right',
+            formatter: function(v) {
+              return `${v.data.name} - ${v.data.value[2]}`
+            }
+          },
+          // 鼠标移入显示label
+          emphasis: {
+            label: {
+              show: true
+            }
+          }
+        },
+        {
+          name: 'Top 2',
+          type: 'effectScatter', //波纹散点
+          coordinateSystem: 'bmap', // 坐标系统
+          data: convertData(
+            scatterData.sort(function(a, b) {
+              return b.value - a.value
+            })
+          ).slice(0, 10),
+          encode: {
+            value: 2 //定制value值
+          },
+          itemStyle: {
+            color: 'purple',
+            shadowBlur: 10,
+            shadowColor: '#333'
+          },
+          symbolSize: function(val) {
+            //点的大小
+            return val[2] / 10
+          },
+          label: {
+            show: false,
+            position: 'right',
+            formatter: function(v) {
+              return `${v.data.name} - ${v.data.value[2]}`
+            }
+          },
+          // 鼠标移入显示label
+          emphasis: {
+            label: {
+              show: true
+            }
+          },
+          hoverAnimation: true,
+          // 波纹样式
+          rippleEffect: {
+            brushType: 'stroke'
+          }
+        }
+      ]
+    }
+  },
+  methods: {
+    afterSet: function(echarts) {
+      var bmap = echarts
+        .getModel()
+        .getComponent('bmap')
+        .getBMap()
+      bmap.setMapStyle(mapStyle)
+    }
+  },
+  mounted() {}
+}
+</script>
+<style lang="scss" scoped></style>
